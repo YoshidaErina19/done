1b070749824540880b650cc56b9fe90395e46028
yoshida_erina <yoshida.erina.19@gmail.com>
2022-06-14 20:38:29 +0900

    買った物記録新規作成ページ(チェックボックスにチェックで登録可)

--- grocery/urls.py
+++ grocery/urls.py
@@ -6,8 +6,8 @@ app_name = 'grocery'
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.GroceryListView.as_view(), name='grocery_list'),
-    path('grocery_create/', views.GroceryCreateView.as_view(), name='grocery_create'),
-    # path('grocery_create/', views.create, name='grocery_create'),
+    # path('grocery_create/', views.GroceryCreateView.as_view(), name='grocery_create'),
+    path('grocery_create/', views.grocery_create, name='grocery_create'),
     path('update/<int:pk>/', views.update, name='update'),
     path('grocery_delete/<int:pk>/', views.GroceryDeleteView.as_view(), name='grocery_delete'),
     ]
--- grocery/views.py
+++ grocery/views.py
@@ -48,6 +48,50 @@ class GroceryCreateView(LoginRequiredMixin, generic.CreateView):
         return super().form_invalid(form)
 
 
+# 関数ベースビュー(チェックしたものが買った物記録として新規作成される)
+def grocery_create(request):
+    initial_values = {"user": request.user}
+
+    checkbox_1 = request.POST.getlist('checkbox_1')
+    checkbox_2 = request.POST.getlist('checkbox_2')
+    checkbox_3 = request.POST.getlist('checkbox_3')
+    form = GroceryCreateForm(request.POST or initial_values)
+    ctx = {"form": form}
+
+    if request.method == 'GET':
+        print(ctx)
+        return render(request, 'grocery/grocery_create.html', ctx)
+
+    if request.method == 'POST':
+        if checkbox_1:
+            checkbox_1 = 'checked'
+        else:
+            checkbox_1 = 'unchecked'
+
+        if checkbox_2:
+            checkbox_2 = 'checked'
+        else:
+            checkbox_2 = 'unchecked'
+
+        if checkbox_3:
+            checkbox_3 = 'checked'
+        else:
+            checkbox_3 = 'unchecked'
+
+        object = Grocery.objects.create(
+            user = request.user,
+            checkbox_1 = checkbox_1,
+            checkbox_2 = checkbox_2,
+            checkbox_3 = checkbox_3,
+            )
+
+        object.save()
+        messages.success(request, '買った物を登録しました。お疲れ様でした！')
+        return redirect('grocery:grocery_list')
+
+    return render(request, 'grocery/grocery_update.html', ctx)
+
+
 def update(request, pk):
     obj = Grocery.objects.get(pk=pk)
     initial_values = {"user": obj.user}
@@ -87,6 +131,7 @@ def update(request, pk):
         return redirect('grocery:grocery_list')
 
     return render(request, 'grocery/grocery_update.html', ctx)
+
 
 class GroceryDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Grocery
