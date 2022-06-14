41078af974e23a4bc9922ba36d5281256b83fc7d
yoshida_erina <yoshida.erina.19@gmail.com>
2022-06-14 16:54:26 +0900

    軽微な修正

--- grocery/urls.py
+++ grocery/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.GroceryListView.as_view(), name='grocery_list'),
     path('grocery_create/', views.GroceryCreateView.as_view(), name='grocery_create'),
     # path('grocery_create/', views.create, name='grocery_create'),
-    path("update/<int:pk>", views.update, name='update'),
+    path('update/<int:pk>/', views.update, name='update'),
+    path('grocery_delete/<int:pk>/', views.GroceryDeleteView.as_view(), name='grocery_delete'),
     ]
