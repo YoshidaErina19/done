8c63f47906cd44310d0cfa9b19c0cea46fa5ec7f
yoshida_erina <yoshida.erina.19@gmail.com>
2022-06-21 21:54:56 +0900

    ログイン機能のテスト作成

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
+from django.urls import reverse_lazy
+from selenium.webdriver.chrome.webdriver import WebDriver
 
-# Create your tests here.
+class TestLogin(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(executable_path='C:/Users/erina/venv_done_housework/chromedriver.exe')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        # ログインページを開く
+        self.selenium.get('http://localhost:8000' + str(reverse_lazy('account_login')))
+
+        # ログイン
+        username_input = self.selenium.find_element_by_name('login')
+        username_input.send_keys('test.user2@example.com')
+        password_input = self.selenium.find_element_by_name('password')
+        password_input.send_keys('neomeito')
+        self.selenium.find_element_by_class_name('btn').click()
+
+        # ページタイトルの検証
+        self.assertEquals('家事一覧｜Done Housework',self.selenium.title)
+
+
+
